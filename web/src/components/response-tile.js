3985bb64d1b268ec54f108af51713b7b8cf72702
saghark <ksaghari0@gmail.com>
2017-02-28 17:09:30 -0500

    Restructured project folders. Front end files now reside in ./web while server code lies in ./server.

---
+++ web/src/components/response-tile.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import EventTimeTable from './event-times-table';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+
+export default class ResponseTile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {responses: []};
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log("Form submitted.");
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Card expanded={true}>
+          <CardHeader
+            title="Response Tile"
+            showExpandableButton={false}
+          />
+          <CardText>
+            <EventTimeTable />
+          </CardText>
+        </Card>
+      </div>
+    );
+  }
+}
