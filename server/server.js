b3f7ba527b7f2e3e28341c2d52b54f8ce2b80547
saghark <ksaghari0@gmail.com>
2017-03-02 14:37:58 -0500

    Small fix to syntax.

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var bodyParser  = require('body-parser');
 var Event       = require('./app/models/event');
 var app = express()
 
-var dbUserName = 'ksaghari';
-var dbPassword = 'testEnvironment767';
+var dbUserName = '';
+var dbPassword = '';
 var dbURI      = 'mongodb://' + dbUserName + ':' + dbPassword + '@ds047622.mlab.com:47622/cas_767_environment';
 
 app.use(bodyParser.urlencoded({extended: true}));
